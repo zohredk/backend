f034c294d07599987e0a13ca226bde0b52c0319b
zohredk <zohrehdk2000@gmail.com>
2024-11-08 23:53:37 +1100

    .

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const productRoutes = require("./routes/product-routes");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  next();
+});
+
 app.use("/api", productRoutes);
 
 app.listen(8000);
